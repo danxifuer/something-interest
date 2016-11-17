7f470ff1f9f7f972746ee7cd54b93452599f7090
daianbo <daianbo@lightinthebox.com>
2016-11-17 14:54:13 +0800

    commit

--- quant/lstm-model-v11-14-predict-future-days.py
+++ quant/lstm-model-v11-14-predict-future-days.py
@@ -58,14 +58,14 @@ class DataHandle:
 
 class LstmModel:
     def __init__(self):
-        filePath = '/home/daiab/code/ml/something-interest/data/000001-minute.csv'
+        filePath = '/home/daiab/code/ml/something-interest/data/000001.csv'
         # filePath = '/home/daiab/code/ml/something-interest/data/601988.csv'
         # filePath = '/home/daiab/code/ml/something-interest/data/000068.csv'
-        self.TIME_STEP = 20
+        self.TIME_STEP = 19
         self.NUM_HIDDEN = 50
         self.epochs = 200
         self._session = tf.Session()
-        self.predictFutureDay = 2
+        self.predictFutureDay = 1
         dataHandle = DataHandle(filePath, self.TIME_STEP)
         self.data = dataHandle.data
         self.trainData = dataHandle.trainData
@@ -92,90 +92,131 @@ class LstmModel:
         # target = np.hsplit(target, [1])[1]
         # print("get_one_epoch_target >>>>>>>>>>>>>>")
         # print(np.array(target))
-        return np.reshape(target, [1, 2])
+        return np.reshape(target, [1, self.predictFutureDay])
 
     def buildGraph(self):
         self.oneTrainData = tf.placeholder(tf.float32, [1, self.TIME_STEP, 4])
-        self.targetPrice = tf.placeholder(tf.float32, [1, 2])
+        self.targetPrice = tf.placeholder(tf.float32, [1, self.predictFutureDay])
+        self.yesterdayPrice = tf.placeholder(tf.float32, [1, self.predictFutureDay])
         cell = tf.nn.rnn_cell.LSTMCell(self.NUM_HIDDEN)
 
         cell_2 = tf.nn.rnn_cell.MultiRNNCell([cell] * 1)
-        val, _ = tf.nn.dynamic_rnn(cell_2, self.oneTrainData, dtype=tf.float32)
+        # cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout)
+        val, self.states = tf.nn.dynamic_rnn(cell_2, self.oneTrainData, dtype=tf.float32)
+        # tf.nn.xw_plus_b()
+        # tf.get_variable()
+
 
         self.val = tf.transpose(val, [1, 0, 2])
-        self.lastTime = tf.gather(self.val, self.TIME_STEP - 1)
+        self.lastTime = tf.gather(self.val, self.val.get_shape()[0] - 1)
 
-
-        self.weight = tf.Variable(tf.truncated_normal([self.NUM_HIDDEN, 2], dtype=tf.float32))
-        self.bias = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[1, 2]))
+        self.weight = tf.Variable(tf.truncated_normal([self.NUM_HIDDEN, self.predictFutureDay], dtype=tf.float32))
+        self.bias = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[1, self.predictFutureDay]))
         self.predictPrice = tf.matmul(self.lastTime, self.weight) + self.bias
+        self.diff = tf.sqrt(tf.reduce_mean(tf.squared_difference(self.predictPrice, self.targetPrice)))
 
-        self.diff = tf.sqrt(tf.reduce_mean(tf.square(self.predictPrice - self.targetPrice)))
+        inse = tf.reduce_sum(self.predictPrice * self.targetPrice)
+        l = tf.reduce_sum(self.predictPrice * self.predictPrice)
+        r = tf.reduce_sum(self.targetPrice * self.targetPrice)
+        self.dice = 2 * inse / (l + r)
 
-        self.minimize = tf.train.AdamOptimizer().minimize(self.diff)
+        # trend = (self.predictPrice - self.yesterdayPrice) * (self.targetPrice - self.yesterdayPrice)
+        # self.sign = tf.reduce_sum(tf.sign(trend))
 
-    def trainModel(self):
-        init_op = tf.initialize_all_variables()
-        self._session.run(init_op)
-        for epoch in range(self.epochs):
-            print("epoch %i >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" % epoch)
-            diffSum = 0
-            count = 0
-            for day in range(self.TIME_STEP, self.trainDays):
-                # print("day %d" % day)
-                realPrice = self.getOneEpochTarget(day)
-                oneEpochTrainData = self.getOneEpochTrainData(day)
+        # tf.clip_by_value()
 
-                self._session.run(self.minimize,
-                                  {self.oneTrainData: oneEpochTrainData,
-                                   self.targetPrice: realPrice})
+        self.minimize = tf.train.AdamOptimizer().minimize(self.diff - self.dice)
+
+    with tf.device("/cpu:0"):
+        def trainModel(self):
+            self._session.run(tf.initialize_all_variables())
+            for epoch in range(self.epochs):
+                print("epoch %i >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" % epoch)
+                diffSum = 0
+                count = 0
+                for day in range(self.TIME_STEP, self.trainDays):
+                    # print("day %d" % day)
+                    oneEpochTrainData = self.getOneEpochTrainData(day)
+                    realPrice = self.getOneEpochTarget(day)
+                    yesterdayPrice = self.getOneEpochTarget(day - 1)
+
+                    self._session.run(self.minimize, {self.oneTrainData: oneEpochTrainData,
+                                                      self.targetPrice: realPrice,
+                                                      self.yesterdayPrice: yesterdayPrice})
+
+                    predictPrice = self._session.run(self.predictPrice,
+                                                     {self.oneTrainData: oneEpochTrainData,
+                                                      self.targetPrice: realPrice,
+                                                      self.yesterdayPrice: yesterdayPrice})
+
+                    diff = self._session.run(self.diff,
+                                                     {self.oneTrainData: oneEpochTrainData,
+                                                      self.targetPrice: realPrice,
+                                                      self.yesterdayPrice: yesterdayPrice})
+
+                    dice = self._session.run(self.dice,
+                                                     {self.oneTrainData: oneEpochTrainData,
+                                                      self.targetPrice: realPrice,
+                                                      self.yesterdayPrice: yesterdayPrice})
+
+                    # states = self._session.run(self.states,
+                    #                          {self.oneTrainData: oneEpochTrainData,
+                    #                           self.targetPrice: realPrice,
+                    #                           self.yesterdayPrice: yesterdayPrice})
+
+
+                    diffSum += diff
+                    count += 1
+
+                    if day % 100 == 0:
+                        print("...................................................")
+                        print("predictPrice is %s" % predictPrice)
+                        print("real price is %s" % realPrice)
+                        print("diff is %s" % diff)
+                        print("dice is %s" % dice)
+                        # print("state is %s" % states)
+                        # print(states.shape)
+                print("diff mean >>>>>>>>>>>>>> %f" % (diffSum / count))
+                if epoch % 20 == 0:
+                    self.test()
+
+    with tf.device("/cpu:1"):
+        def test(self):
+            predict = np.zeros([1])
+            real = np.zeros([1])
+            dayIndex = [self.trainDays - 1]
+
+            for day in range(self.trainDays, self.days - self.predictFutureDay):
+                trainData = self.getOneEpochTrainData(day)
+                # target = self.getOneEpochTarget(day)
 
                 predictPrice = self._session.run(self.predictPrice,
-                                                 {self.oneTrainData: oneEpochTrainData,
-                                                  self.targetPrice: realPrice})
+                                                 {self.oneTrainData: trainData})[:, 0]
 
-                diff = self._session.run(self.diff,
-                                         {self.oneTrainData: oneEpochTrainData,
-                                          self.targetPrice: realPrice})
-                diffSum += diff
-                count += 1
+                realPrice = self.getOneEpochTarget(day)[:, 0]
 
-                if day % 100 == 0:
-                    print("...................................................")
-                    print("predictPrice is %s" % predictPrice)
-                    print("real price is %s" % self.getOneEpochTarget(day))
-                    print("diff is %s" % diff)
-            print("diff mean >>>>>>>>>>>>>> %f" % (diffSum / count))
-            if epoch % 20 == 0:
-                self.test()
+                # print(">>>>>>>>>>>>>>>,,,,,,,,,,,,, day %d" % day)
+                # print(trainData)
+                # print(target)
+                # print(realPrice)
 
-    def test(self):
-        predict = np.zeros([1])
-        real = np.zeros([1])
-        dayIndex = [self.trainDays - 1]
-        for day in range(self.trainDays, self.days - self.predictFutureDay):
-            predictPrice = self._session.run(self.predictPrice,
-                                              {self.oneTrainData: self.getOneEpochTrainData(day),
-                                               self.targetPrice: self.getOneEpochTarget(day)})[:, 0]
-            realPrice = self.getOneEpochTarget(day-1)[:, 0]
+                # print(predict_price)
+                predict = np.concatenate([predict, predictPrice])
+                # print(predict)
+                real = np.concatenate([real, realPrice])
+                # print(real)
+                dayIndex.append(day)
+            # print(predict[:, 0])
+            if self.isPlot:
+                self.plotLine(dayIndex[1:], predict[1:], real[1:])
 
-            # print(predict_price)
-            predict = np.concatenate([predict, predictPrice])
-            # print(predict)
-            real = np.concatenate([real, realPrice])
-            # print(real)
-            dayIndex.append(day)
-        # print(predict[:, 0])
-        if self.isPlot:
-            self.plotLine(dayIndex[1:], predict[1:], real[1:])
-
-
-    def plotLine(self, days, predict, real):
-        plt.ylabel("close")
-        plt.grid(True)
-        plt.plot(days, predict, 'r-')
-        plt.plot(days, real, 'b-')
-        plt.show()
+    with tf.device("/cpu:3"):
+        def plotLine(self, days, predict, real):
+            plt.ylabel("close")
+            plt.grid(True)
+            plt.plot(days, predict, 'r-')
+            plt.plot(days, real, 'b-')
+            plt.show()
 
 
     def run(self):
